ccec7f8979a93aaebf5b5ae62191bc1831f8483c
sengkimin <sengin.pse@gmail.com>
2025-03-19 14:33:04 +0700

    3page

---
+++ lib/views/login.dart
@@ -0,0 +1,154 @@
+import 'package:flutter/material.dart';
+import 'package:trash_panda/views/home.dart';
+import 'package:trash_panda/views/register.dart';
+
+class Login extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          Positioned(
+            top: -200,
+            right: -200,
+            child: Container(
+              width: 400,
+              height: 400,
+              decoration: const BoxDecoration(
+                color: Color(0xFF80AF81),
+                shape: BoxShape.circle,
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: -200,
+            left: -200,
+            child: Container(
+              width: 400,
+              height: 400,
+              decoration: const BoxDecoration(
+                color: Color(0xFF80AF81),
+                shape: BoxShape.circle,
+              ),
+            ),
+          ),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 35),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Hello!',
+                    style: TextStyle(
+                      fontSize: 45,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF058B09),
+                    ),
+                  ),
+                  const SizedBox(height: 4),
+                  const Text(
+                    'Welcome to Trash Panda',
+                    style: TextStyle(
+                      fontSize: 22,
+                      color: Color(0xFF058B09),
+                    ),
+                  ),
+                  const SizedBox(height: 35),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 10),
+                    child: const Text(
+                      'Login',
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFF416944),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 15),
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Email',
+                      hintStyle:
+                          TextStyle(fontSize: 18, color: Color(0xFF6FAD67)),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 15),
+                  TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: 'Password',
+                        hintStyle:
+                          TextStyle(fontSize: 18, color: Color(0xFF6FAD67)),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 15),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: Padding(
+                      padding: const EdgeInsets.only(bottom: 30),
+                      child: TextButton(
+                        onPressed: () {},
+                        child: const Text(
+                          'Forgot Password',
+                          style: TextStyle(color: Colors.green, fontSize: 18),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFF416944),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(24)),
+                        padding: const EdgeInsets.symmetric(vertical: 14),
+                      ),
+                      onPressed: () {
+                        Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => HomePage()));
+                      },
+                      child: const Text('Login',
+                          style: TextStyle(fontSize: 20, color: Colors.white)),
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+                  Center(
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 20),
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => SignUp()));
+                        },
+                        child: const Text("Don’t have an account? Sign up",
+                            style: TextStyle(
+                                fontWeight: FontWeight.w300,
+                                color: Color(0xFF6FAD67),
+                                fontSize: 18)),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
